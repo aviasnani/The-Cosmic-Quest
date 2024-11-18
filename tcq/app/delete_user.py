0669c87c610a6b1f0ea19153fb807e19fc914e4f
AviAsnani <aviasnani@Avis-MacBook-Air.local>
2024-11-18 08:48:08 +0530

    modified delete_user, fixed errors

--- tcq/app/delete_user.py
+++ tcq/app/delete_user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, url_for, render_template
 from .models import db, User
 
 delete_user_bp = Blueprint('delete_user', __name__)
@@ -6,7 +6,9 @@ delete_user_bp = Blueprint('delete_user', __name__)
 @delete_user_bp.route('/delete_user/<int:user_id>', methods=['GET', 'POST'])
 def delete_user(user_id):
     user = User.query.get(user_id)
+
     if user:
         db.session.delete(user)
         db.session.commit()
-        return redirect(url_for('login'))
+        return redirect(url_for('login.login'))
+    return render_template('profile.html', user_id=user_id)
